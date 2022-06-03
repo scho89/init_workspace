7cd88e89d528d8c713187d66a3651d581b5e4444
Sangho Cho <cho.sangho@outlook.com>
2022-06-03 16:46:05 +0900

    modify date format in prompt '-' to '/'

--- cmp.ps1
+++ cmp.ps1
@@ -9,7 +9,7 @@ try {
 
     function global:prompt {
 
-        $prefix = "["+(Get-Date -format "MM-dd HH:mm:ss")+"] "
+        $prefix = "["+(Get-Date -format "MM'/'dd HH:mm:ss")+"] "
         Write-Host $prefix -f yellow -nonewline
         Write-Host $global:o365Service -f Green -NoNewline
         Write-Host $global:tenant -f red -NoNewline
